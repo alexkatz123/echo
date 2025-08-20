6b4af12848ab4b0afd44ca5c530818c92ff9e6c5
Alex Katz <katzalex01@gmail.com>
2025-08-20 20:42:46 +0100

    03 clerk authetication

---
+++ apps/web/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+const Layout = ({ children}: { children: React.ReactNode }) => {
+    return (
+        <div className="min-h-screen min-w-screen flex h-full flex-col items-center justify-center">
+            {children}
+        </div>
+    );
+}
+ 
+export default Layout;
--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -1,19 +1,23 @@
 import { mutation, query } from "./_generated/server";
 
 export const getMany = query({
-    args: {},
-    handler: async (ctx) => {
-        const users = await ctx.db.query("users").collect();
-        return users;
-    }
-})
+  args: {},
+  handler: async (ctx) => {
+    const users = await ctx.db.query("users").collect();
+    return users;
+  },
+});
 
 export const add = mutation({
-    args: {  },
-    handler: async (ctx) => {
-        const userId = await ctx.db.insert("users", { 
-            name: "Alex"
-          });
-        return userId;
+  args: {},
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (identity === null) {
+      throw new Error("Not authenticated");
     }
-})
+    const userId = await ctx.db.insert("users", {
+      name: "Alex",
+    });
+    return userId;
+  },
+});
