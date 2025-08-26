079d13bc385df92275f67f1d1454e11cdc46c3b7
Alex Katz <katzalex01@gmail.com>
2025-08-26 02:31:21 +0100

    07 dashboard layout

---
+++ apps/web/app/(dashboard)/billing/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+    return (
+        <div>
+            <h1>billing</h1>
+        </div>
+    );
+}
+ 
+export default Page;
---
+++ apps/web/app/(dashboard)/files/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+    return (
+        <div>
+            <h1>files</h1>
+        </div>
+    );
+}
+ 
+export default Page;
