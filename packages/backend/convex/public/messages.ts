2680305e8db44922ad4f9eda9a4fe9fd86b9add6
Alex Katz <katzalex01@gmail.com>
2025-08-31 20:52:20 +0100

    19 ai tool calling

--- packages/backend/convex/public/messages.ts
+++ packages/backend/convex/public/messages.ts
@@ -1,84 +1,99 @@
 import { ConvexError, v } from "convex/values";
 import { action, query } from "../_generated/server";
-import { internal } from "../_generated/api";
+import { components, internal } from "../_generated/api";
 import { supportAgent } from "../system/ai/agents/supportAgent";
 import { paginationOptsValidator } from "convex/server";
+import { resolveConversation } from "../system/ai/tools/resolveConversation";
+import { escalateConversation } from "../system/ai/tools/escalateConversation";
+import { saveMessage } from "@convex-dev/agent";
 
 export const create = action({
-    args: {
-        prompt: v.string(),
-        threadId: v.string(),
-        contactSessionId: v.id("contactSessions")
-    },
-    handler: async (ctx, args) => {
-        const contactSession = await ctx.runQuery(
-            internal.system.contactSessions.getOne,
-            {
-                contactSessionId: args.contactSessionId
-            }
-        )
+  args: {
+    prompt: v.string(),
+    threadId: v.string(),
+    contactSessionId: v.id("contactSessions"),
+  },
+  handler: async (ctx, args) => {
+    const contactSession = await ctx.runQuery(
+      internal.system.contactSessions.getOne,
+      {
+        contactSessionId: args.contactSessionId,
+      }
+    );
 
-        if (!contactSession || contactSession.expiresAt < Date.now()) {
-            throw new ConvexError({
-                code: "NOT_FOUND",
-                message: "Contact session not found"
-            })
-        }
-
-        const conversation = await ctx.runQuery(
-            internal.system.conversations.getByThreadId,
-            {
-                threadId: args.threadId
-            }
-        )
-
-        if (!conversation) {
-            throw new ConvexError({
-                code: "NOT_FOUND",
-                message: "Conversation not found"
-            })
-        }
-
-        if (conversation.status === "resolved") {
-            throw new ConvexError({
-                code: "BAD_REQUEST",
-                message: "Conversation is already resolved"
-            })
-        }
-
-        //TODO subscription check
-
-        await supportAgent.generateText(
-            ctx,
-            { threadId: args.threadId },
-            {
-                prompt: args.prompt,
-            }
-        )
+    if (!contactSession || contactSession.expiresAt < Date.now()) {
+      throw new ConvexError({
+        code: "NOT_FOUND",
+        message: "Contact session not found",
+      });
     }
-})
+
+    const conversation = await ctx.runQuery(
+      internal.system.conversations.getByThreadId,
+      {
+        threadId: args.threadId,
+      }
+    );
+
+    if (!conversation) {
+      throw new ConvexError({
+        code: "NOT_FOUND",
+        message: "Conversation not found",
+      });
+    }
+
+    if (conversation.status === "resolved") {
+      throw new ConvexError({
+        code: "BAD_REQUEST",
+        message: "Conversation is already resolved",
+      });
+    }
+
+    //TODO subscription check
+    const shouldTriggerAgent = conversation.status === "unresolved";
+
+    if (shouldTriggerAgent) {
+      await supportAgent.generateText(
+        ctx,
+        { threadId: args.threadId },
+        {
+          prompt: args.prompt,
+          tools: {
+            resolveConversation,
+            escalateConversation,
+          },
+        }
+      );
+    } else {
+        await saveMessage(ctx, components.agent, {
+            threadId: args.threadId,
+            prompt: args.prompt,
+        });
+    }
+  },
+});
 
 export const getMany = query({
-    args: {
-        threadId: v.string(),
-        paginationOpts: paginationOptsValidator,
-        contactSessionId: v.id("contactSessions")
-    },
-    handler: async (ctx, args) => {
-        const contactSession = await ctx.db.get(args.contactSessionId)
+  args: {
+    threadId: v.string(),
+    paginationOpts: paginationOptsValidator,
+    contactSessionId: v.id("contactSessions"),
+  },
+  handler: async (ctx, args) => {
+    const contactSession = await ctx.db.get(args.contactSessionId);
 
-        if (!contactSession || contactSession.expiresAt < Date.now()) {
-            throw new ConvexError({
-                code: "UNAUTHORIZED",
-                message: "Contact session is invalid"
-            })
-        }
-
-        const paginated = await supportAgent.listMessages(ctx, {
-            threadId: args.threadId,
-            paginationOpts: args.paginationOpts,
-        })
-
-        return paginated;
+    if (!contactSession || contactSession.expiresAt < Date.now()) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Contact session is invalid",
+      });
     }
-})
+
+    const paginated = await supportAgent.listMessages(ctx, {
+      threadId: args.threadId,
+      paginationOpts: args.paginationOpts,
+    });
+
+    return paginated;
+  },
+});
--- packages/backend/convex/system/ai/agents/supportAgent.ts
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -4,5 +4,5 @@ import { components } from "../../../_generated/api"
 
 export const supportAgent = new Agent(components.agent, {
     chat: openai.chat("gpt-4o-mini"),
-    instructions: "You are a customer support agent",
+    instructions: `You are a customer support agent. Use "resolveConversation" tool when user expresses finalization of the conversation. use "escalateConversation" tool when user expresses frustration or requests a human explicitly`,
 });
