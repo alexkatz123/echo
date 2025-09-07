f650d37ddaa366f372cea02f57f5aa03cff7b36b
Alex Katz <katzalex01@gmail.com>
2025-09-07 03:06:49 +0100

    34 embed script

--- apps/web/modules/integrations/constants.ts
+++ apps/web/modules/integrations/constants.ts
@@ -23,7 +23,7 @@ export const INTEGRATIONS = [
 
 export type IntegrationId = (typeof INTEGRATIONS)[number]["id"];
 
-export const HTML_SCRIPT = `<script data-organization-id="{{ORGANIZATION_ID}}">html</script>`;
-export const REACT_SCRIPT = `<script data-organization-id="{{ORGANIZATION_ID}}">react</script>`;
-export const JAVASCRIPT_SCRIPT = `<script data-organization-id="{{ORGANIZATION_ID}}">javascript</script>`;
-export const NEXTJS_SCRIPT = `<script data-organization-id="{{ORGANIZATION_ID}}">nextjs</script>`;
+export const HTML_SCRIPT = `<script src="http://localhost:3001/widget.js" data-organization-id="{{ORGANIZATION_ID}}">html</script>`;
+export const REACT_SCRIPT = `<script src="http://localhost:3001/widget.js" data-organization-id="{{ORGANIZATION_ID}}">react</script>`;
+export const JAVASCRIPT_SCRIPT = `<script src="http://localhost:3001/widget.js" data-organization-id="{{ORGANIZATION_ID}}">javascript</script>`;
+export const NEXTJS_SCRIPT = `<script src="http://localhost:3001/widget.js" data-organization-id="{{ORGANIZATION_ID}}">nextjs</script>`;
